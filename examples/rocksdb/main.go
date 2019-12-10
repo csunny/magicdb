86c3538d84eb66676776e96c74de587524256766
csunny <cfqsunny@163.com>
2019-12-10 21:06:01 +0800

    Storage: init storage and add rely lib

---
+++ examples/rocksdb/main.go
@@ -0,0 +1,45 @@
+// Copyright 2019 The magicdb Authors
+//
+// Licensed under the Apache Licence, Version 2.0(the "License");
+// You may not use the file except in compliance with the Licence.
+// You may obtain a copy of the Licence at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distrubuted under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+/*
+#cgo CFLAGS: -I/usr/local/lib/rocksdb/include
+#cgo LDFLAGS: -L/usr/local/lib/rocksdb -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -llz4 -lzstd
+*/
+import "C"
+import (
+	"fmt"
+
+	"github.com/tecbot/gorocksdb"
+)
+
+var tmpPath = "/tmp/magicdb"
+
+func main() {
+	bbto := gorocksdb.NewDefaultBlockBasedTableOptions()
+	bbto.SetBlockCache(gorocksdb.NewLRUCache(3 << 30))
+
+	opts := gorocksdb.NewDefaultOptions()
+	opts.SetBlockBasedTableFactory(bbto)
+
+	opts.SetCreateIfMissing(true)
+	db, err := gorocksdb.OpenDb(opts, tmpPath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer db.Close()
+
+}
--- src/storage/README.md
+++ src/storage/README.md
@@ -3,3 +3,15 @@
 At the first, use rocksdb impl a single node k-v storage server, and then use libp2p raft to rewrite.
 
 go go go !!!
+
+
+# rocksdb install
+
+## Mac Install  
+> brew install rocksdb
+
+### Install gorocksdb
+
+CGO_CFLAGS="-I/usr/local/lib/rocksdb/include" \
+CGO_LDFLAGS="-L/usr/local/lib/rocksdb -lrocksdb -lstdc++ -lm -lz -lbz2 -lsnappy -llz4 -lzstd" \
+  go get github.com/tecbot/gorocksdb

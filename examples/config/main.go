3642b54a104dcec1ac6ab2148ffc503e5c226adf
csunny <cfqsunny@163.com>
2019-12-12 19:55:52 +0800

    CI/CD add travis file

---
+++ examples/config/main.go
@@ -0,0 +1,21 @@
+// Copyright 2019 The magicdb Authors
+//
+// Licensed under the Apache Licence, Version 2.0(the "License");
+// You may not use the file except in compliance with the Licence.
+// You may obtain a copy of the Licence at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distrubuted under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello world")
+}
